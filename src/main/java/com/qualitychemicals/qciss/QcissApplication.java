061e491c7e25d44e1e84f1d9a1eb99083dcccdce
lodkazibwe <lodkazibwe@gmail.com>
2020-11-24 11:59:05 +0300

    jar changes

--- src/main/java/com/qualitychemicals/qciss/QcissApplication.java
+++ src/main/java/com/qualitychemicals/qciss/QcissApplication.java
@@ -2,16 +2,9 @@ package com.qualitychemicals.qciss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QcissApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(QcissApplication.class);
-    }
+public class QcissApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QcissApplication.class, args);
