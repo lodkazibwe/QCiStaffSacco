5151d18eef82e742f6c00392b2f64e282531f8b5
lodkazibwe <lodkazibwe@gmail.com>
2021-09-12 17:24:53 +0300

    cumulative

--- src/main/java/com/qualitychemicals/qciss/transaction/dto/CumulativeShareT.java
+++ src/main/java/com/qualitychemicals/qciss/transaction/dto/CumulativeShareT.java
@@ -1,15 +1,20 @@
 package com.qualitychemicals.qciss.transaction.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class CumulativeShareT {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", locale = "pt-BR", timezone = "EAT")
+    @Temporal(TemporalType.TIMESTAMP)
     private Date creationDateTime;
     private double amount;
     private String account;
